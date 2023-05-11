707578b122b95af176b7330f0fd9a6ff6e86317d
shash123-d <88714649+shash123-d@users.noreply.github.com>
2023-05-11 09:01:10 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -14,3 +14,16 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+if ! [ -f student-submission/ListExamples.java ]; then
+    exit
+fi
+
+cp TestListExamples.java grading-area
+cp student-submission/ListExamples.java grading-area
+
+
+javac ListExamples.java TestListExamples.java
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore grading-area/TestListExamples
